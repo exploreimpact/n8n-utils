d67f2074af989ca44ed2e166588a45577be0adc4
Claudio De Facci <claudio@exploreimpact.de>
2026-09-07 16:34:00 +0200

    Release.

--- src/utils/api-exception.js
+++ src/utils/api-exception.js
@@ -1,13 +1,20 @@
-function createApiExceptionResponse(message, code, context) {
-  return {
+function throwApiException(message, code, context) {
+  const errorObject = {
     ok: false,
     code: code ?? 500,
     message: message,
     context: context ?? {},
     // ...
   };
+
+  throw new Error(JSON.stringify(errorObject));
+}
+
+function parseApiException(jsonString) {
+  return JSON.parse(jsonString);
 }
 
 module.exports = {
-  createApiExceptionResponse: createApiExceptionResponse,
+  throwApiException,
+  parseApiException,
 };
